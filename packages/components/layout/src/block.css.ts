45371e3ffdc5d87a2131162081ff037a52ac006b
dainiscamel <juhyueon3@naver.com>
2024-12-07 23:44:30 +0900

    feat vanila extract 추가

---
+++ packages/components/layout/src/block.css.ts
@@ -0,0 +1,7 @@
+import { tokens } from "@pesto-ui/themes";
+import { style } from "@vanilla-extract/css";
+export const blockStyle = style({
+  width: "100px",
+  height: "100px",
+  backgroundColor: tokens.colors.$scale.blue[500],
+});
