0585a2ddfd7ad4957a41a5e94a4766b766ca6997
dainiscamel <juhyueon3@naver.com>
2024-12-09 02:00:25 +0900

    feat storybook 추가

---
+++ services/storybook/.storybook/preview.js
@@ -0,0 +1,45 @@
+/** @type { import('@storybook/react').Preview } */
+import "./style.css";
+
+const preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+  rootAttributesTooltip: true,
+  rootAttribute: {
+    root: "body",
+    attribute: "class",
+    defaultState: {
+      name: "Light",
+      value: "theme-light",
+    },
+    states: [
+      {
+        name: "Dark",
+        value: "theme-dark",
+      },
+    ],
+  },
+};
+const initTheme = () => {
+  const isDarkTheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  if (isDarkTheme) {
+    document.body.classList.add("theme-dark");
+  }
+  const mediaQueryList = window.matchMedia("(prefers-color-scheme: dark)");
+  mediaQueryList.addEventListener("change", (e) => {
+    if (e.matches) {
+      document.body.classList.add("theme-dark");
+    } else {
+      document.body.classList.remove("theme-dark");
+    }
+  });
+};
+initTheme();
+
+export default preview;
---
+++ services/storybook/stories/layout/Block.stories.tsx
@@ -0,0 +1,13 @@
+import "@pesto-ui/components-layout/style.css";
+import { Block as _Block } from "@pesto-ui/components-layout";
+
+export default {
+  title: "sComponents/Layout/Block",
+  component: _Block,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+};
+
+export const BlockStory = {};
