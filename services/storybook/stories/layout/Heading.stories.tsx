53618a618165638233a6d32fd73dd6325c2493cf
dainiscamel <juhyueon3@naver.com>
2024-12-13 18:03:52 +0900

    feat typhography-Heading 추가

---
+++ services/storybook/stories/layout/Heading.stories.tsx
@@ -0,0 +1,20 @@
+import "@pesto-ui/components-layout/style.css";
+import { Heading as _Heading } from "@pesto-ui/components-layout";
+
+export default {
+  title: "sComponents/Layout/Heading",
+  component: _Heading,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+};
+
+export const HeadingStory = {
+  args: {
+    as: "h1",
+    fontSize: "sm",
+    children: "This is H1",
+    color: "orange",
+  },
+};
